5e2956d21a7c1f3f3853aa18a668b595fc40bcbe
Gary Großgarten <gary.grossgarten@gmail.com>
2019-03-02 18:27:15 +0100

    refactor(core): improve app architecture and names

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var plugin_1 = require("./plugin");
-var plugin_2 = require("./plugin");
-exports.CorePlugin = plugin_2.CorePlugin;
-exports.default = plugin_1.CorePlugin;
+exports.CorePlugin = plugin_1.CorePlugin;
